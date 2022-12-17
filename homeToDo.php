b3092edc029c133392d9f93c022009e34ef3581e
Guido <guidocalciano@gmail.com>
2022-12-17 15:46:38 -0300

    actualizada hoja estilos

--- homeToDo.php
+++ homeToDo.php
@@ -5,16 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDOs List</title>
-    <link rel="stylesheet" href="./css/style3.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 </head>
-</head>
+
 <body>
-    <div class="toDosName - color-primary" >
+  <div class="container">
+
+    <div class="toDosName color-primary" >
         <h1>Nombre Lista de ToDos</h1>
         <h2>blbalba  items incompletos</h2>
     </div>    
+
+
     <form class="row g-3">        
         <div class="mb-3">
             <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Tipee una tarea aqui">
@@ -22,7 +27,9 @@
                 <button type="submit" class="btn btn-primary mb-3">Agregar</button>
               </div>
         </div>
-      </form>
+    </form>
+
+
     <table class="table caption-top">
         <thead>
           <tr>
@@ -64,17 +71,16 @@
           </tr>
 
         </tbody>
-      </table>
-      <div>
-        <div class="tareasRealizadas">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault"> Mostrar tareas realizadas        
-            </label></td>
-      </div>
+    </table>
+
+    <div class="tareasRealizadas">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault"> Mostrar tareas realizadas        
+        </label></td>
     </div>
 
 
-
+  </div>
 
     
 
@@ -83,8 +89,8 @@
 
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"></script> -->
 
 </html>

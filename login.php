ed34c276eea5c00c9e19dcedbc10029f20a18213
Guido <guidocalciano@gmail.com>
2022-12-17 13:59:25 -0300

    login ahora redirije a To Do Page

--- login.php
+++ login.php
@@ -5,8 +5,7 @@ require 'connectDB.php';
 $connect = conectar();
 
 if ($connect) {
-    // me traigo los datos tipeados en el form
-    // $id = $_POST['id'];
+
     $passOriginal=  $_POST['pass'];
     $pass = md5($passOriginal);
     $usuario = $_POST['usuario'];
@@ -20,32 +19,10 @@ if ($connect) {
         print ("Datos de login equivocados <br>");
         print "El usuario '".$usuario . "' no está registrado con la contraseña ingresada.<br>";
 
-
-    }else {
-        print "Datos Autenticados! <br>";
-        print "Bienvenido '".$usuario . "'!<br>";
-
-        print "<br>Aqui irá el To Do List";
-
-        // ESTO ES LO QUE LEVANTABA LA TABLA DE JUGADORES, PUEDE SERVIR DE MODELO
-
-        // $instruccion2="Select * from jugador";
-        // $consulta2=mysqli_query($connect,$instruccion2) or die ("Fallo en la consulta");
-        // $filas2=mysqli_num_rows($consulta2);
-
-        //         // print ("Datos Autenticados! Lista de jugadores: <br>");
-        // print "<br>";
-        // print "<table border=1>";
-        // print "<tr><th>ID</th><th>Nombre</th><th>Edad</th><th>Camiseta</th><th>Club</th></tr>";
-        // while ($fila=mysqli_fetch_array($consulta2)){
-        //     print "<tr>
-        //        <td>".$fila['idjugador']."</td>
-        //        <td>".$fila['nombre']."</td>
-        //        <td>".$fila['edad']."</td>
-        //        <td>".$fila['camiseta']."</td>
-        //        <td>".$fila['club']."</td>
-        //        </tr>";
-        // }
+    } else {
+        // print "Datos Autenticados! <br>";
+        // print "Bienvenido '".$usuario . "'!<br>";
+        header("Location: homeToDo.php");
 
     }
 

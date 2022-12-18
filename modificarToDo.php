64fed668db583e9b0a6bc8fc27188ecd6f8e6446
WallyBautista21 <112522074+WallyBautista21@users.noreply.github.com>
2022-12-17 22:47:26 -0300

    Update modificarToDo.php

--- modificarToDo.php
+++ modificarToDo.php
@@ -6,9 +6,9 @@ $connect = conectar();
 if ($connect) {
     // me traigo los datos tipeados en el form por el empleado
     $ID=$_POST['ID'];
-    $descripcion= $_POST['descripcion']; 
-
-    $sql="UPDATE tpasap.tareas set ID='".$ID."', descripcion='".$descripcion."' where ID=".$ID;
+    $descripcion= $_POST['descripcion'];
+    $realizado = $_POST['realizado'];
+    $sql="UPDATE tpasap.tareas set ID='".$ID."', descripcion='".$descripcion."', realizado='".$realizado."' where ID=".$ID;
 
     $rpta = mysqli_query($connect, $sql) or die(mysqli_error($connect));
     print "La Tarea  $ID  fue modificada";
